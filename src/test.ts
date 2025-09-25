340c3a8f88c2375d644a9389b804a6061dd8b579
BXSoft <jeremiahlowinne@gmail.com>
2025-09-25 21:04:09 +0200

    minor fix

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { LoginPageComponent } from './app/features/auth/login-page/login-page.component';
+import { LoginPageComponent } from './app/features/auth/login-page.component';
 import { AuthService } from './app/core/services/auth.service';
 
 class MockAuthService {
